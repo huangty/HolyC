0fd7749ad8ac6a141a57a467668ded6fcf6a32b1
ykk <ykk@coin.stanford.edu>
2011-04-19 21:37:29 -0700

    Remove unwanted key

--- src/net/holyc/HolyCIntent.java
+++ src/net/holyc/HolyCIntent.java
@@ -12,14 +12,12 @@ package net.holyc;
 public class HolyCIntent{
     public class BroadcastOFEvent{
 	public static final String action = "holyc.intent.broadcast.OFEVENT";
-	//public static final String bundle_key = "MSG_OFCOMM_EVENT";
 	public static final String str_key = "OFEVENT";
 	
     }
 
     public class BroadcastOFReply{
 	public static final String action = "holyc.intent.broadcast.OFREPLYEVENT";
-	//public static final String bundle_key = "OF_REPLY_EVENT";
 	public static final String str_key = "OF_REPLY_EVENT";
     }
 }
