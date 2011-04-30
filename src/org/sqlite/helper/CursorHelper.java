dcd9dc20fdf4dde9a6184d77b87be407724a5c62
ykk <ykk@coin.stanford.edu>
2011-04-30 02:11:38 -0700

    Wrap SQLite result and have helper function for deciphering

--- src/org/sqlite/helper/CursorHelper.java
+++ src/org/sqlite/helper/CursorHelper.java
@@ -4,11 +4,16 @@ import android.database.sqlite.SQLiteCursor;
 
 import java.util.Vector;
 
+import com.google.gson.Gson;
+
+
 /** Class to help with SQLiteCursor
  * @author ykk
  */
 public class CursorHelper
 {
+    public static Gson gson = new Gson();
+
     /** Return appropriate object for column
      *
      * @return String for string;
@@ -29,18 +34,58 @@ public class CursorHelper
 	
 	return null;
     }
+
+    /** Return appropriate string for object in column
+     */
+    public static String getString(SQLiteCursor c, int columnIndex)
+    {
+	if (c.isNull(columnIndex))
+	    return "N";
+	
+	if (c.isString(columnIndex))
+	    return "S"+gson.toJson(c.getString(columnIndex),
+				   String.class);
+
+	if (c.isFloat(columnIndex))
+	    return "D"+gson.toJson(new Double(c.getDouble(columnIndex)),
+				   Double.class);
+
+	if (c.isLong(columnIndex))
+	    return "L"+gson.toJson(new Long(c.getLong(columnIndex)),
+				   Long.class);
+	
+	return "N";
+    }
     
     /** Return vector with appropriate objects for a row
      */
-    public static Vector getRow(SQLiteCursor c)
+    public static Vector<String> getRow(SQLiteCursor c)
     {
-	Vector v = new Vector();
+	Vector<String> v = new Vector<String>();
 	int count = c.getColumnCount();
 	for (int i = 0; i < count; i++)
-	    v.add(CursorHelper.getObject(c,i));
+	    v.add(CursorHelper.getString(c,i));
 	
 	return v;
     }
     
-				   
+    /** Interpret row
+     */
+    public static Vector decipherRow(Vector<String> in)
+    {
+	Vector v = new Vector();
+	for (int i = 0; i < in.size(); i++)
+	{
+	    String s = (String) in.get(i);
+	    if (s.startsWith("N"))
+		v.add(null);
+	    else if (s.startsWith("S"))
+		v.add(gson.fromJson(s.substring(1), String.class));
+	    else if (s.startsWith("D"))
+		v.add(gson.fromJson(s.substring(1), Double.class));
+	    else if (s.startsWith("L"))
+		v.add(gson.fromJson(s.substring(1), Long.class));
+	}
+	return v;
+    }
 }
