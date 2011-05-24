beebcd67388b1fb64d3c783c1ad978caaf9a05cd
ykk <ykk@coin.stanford.edu>
2011-05-23 19:18:40 -0700

    Adding parameters for request
    Fixing my stupidity

--- src/edu/stanford/lal/LalFlowSwitch.java
+++ src/edu/stanford/lal/LalFlowSwitch.java
@@ -24,13 +24,35 @@ public class LalFlowSwitch extends FlowSwitch {
 	 * Log name
 	 */
 	private String TAG = "Lal.FlowSwitch";
+        /**
+	 * Local port number
+	 */
+        public static short LOCAL_PORT = 1;
+
 	/**
 	 * Hashmap for app name
 	 */
 	public static HashMap<String, Object> appNames = new HashMap<String, Object>();
 
 	public int getCookie(OFMatch ofm, Context context) {
-	    Utility.sendQueryRequest();
+	    if(ofm.getNetworkProtocol() == 0x06 || ofm.getNetworkProtocol() == 0x11){
+		//it is tcp or udp
+		String remoteIP = "";
+		int remotePort = 0;
+		int localPort = 0;
+
+		if (ofm.getInputPort() == LOCAL_PORT) {
+		    remoteIP = ipToString(ofm.getNetworkDestination());
+		    remotePort = U16.f(ofm.getTransportDestination());
+		    localPort = U16.f(ofm.getTransportSource());
+		} else {
+		    remoteIP = ipToString(ofm.getNetworkSource());
+		    remotePort = U16.f(ofm.getTransportSource());
+		    localPort = U16.f(ofm.getTransportDestination());
+		}
+		Utility.sendQueryRequest(remoteIP, remotePort,
+					 localPort, context);
+
 	    return 0;
 	}
 
